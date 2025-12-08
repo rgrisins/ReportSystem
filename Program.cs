95c47b7e78ecfd2ef75783fedafab6b15f28defe
rgrisins <rgrisins@gmail.com>
2025-12-08 09:53:15 +0200

    Fix HTTPS redirection

--- Program.cs
+++ Program.cs
@@ -95,11 +95,9 @@ using (var scope = app.Services.CreateScope())
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    app.UseHttpsRedirection();
     app.UseHsts();
 }
-
-// Middleware configuration
-app.UseHttpsRedirection();
 
 app.UseRouting();
 
